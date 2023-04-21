82778683a25939310f5a0b4684fb26e8fbdc5496
Meet <meetmungra3@gmail.com>
2023-04-21 23:33:32 +0530

    signup login forget pass

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+
+const routes: Routes = [
+{
+  component:SignupComponent,
+  path:"signup"
+},
+{
+  component:LoginComponent,
+  path:"login" 
+},
+{
+  component:ForgotPasswordComponent,
+  path:"forgot-password"
+},
+{path:"",component:LoginComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
